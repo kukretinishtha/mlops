4327c58fe98b2b81a5c2114a7e8c796188c9dbab
kukretinishtha <nishthakukreti.01@gmail.com>
2023-04-19 17:02:41 +0530

    added files

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # Create resource group
 az group create --name pythonwebapp --location westus
 # Create service plan
-az appservice plan create --resource-group  pythonwebapp --name pythonwebappplan --is-linux --sku B1
+az appservice plan create --resource-group  pythonwebapp --name pythonwebappplan --is-linux --sku F1
 # Create a service where we will be deploying
-aaz webapp create --resource-group pythonwebapp --plan pythonwebappplan --name pythonwebapptest --runtime "Python|3.7"
+az webapp create --resource-group pythonwebapp --plan pythonwebappplan --name pythonwebapptest --runtime "Python|3.7"
